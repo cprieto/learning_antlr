480954a15314fb2461e7dc6a7fbd91a6081ffa36
Cristian Prieto <me@cprieto.com>
2016-05-06 16:46:07 +1000

    Well, we have a test program written in Kotlin

--- lesson03/Test.kt
+++ lesson03/Test.kt
@@ -1,6 +1,12 @@
 import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.runtime.CommonTokenStream
 
 fun main(args: Array<String>) {
-    val input = ANTLRInputStream(System.`in`);
-    println("Hello world!")
+    val input = ANTLRInputStream(System.`in`)
+    val lexer = ArrayInitLexer(input)
+    val tokens = CommonTokenStream(lexer)
+    val parser = ArrayInitParser(tokens)
+    val tree = parser.init()
+
+    println(tree.toStringTree(parser))
 }
