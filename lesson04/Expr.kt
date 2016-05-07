762dfad0d35d6243c000a7b343b5530648f19dc2
Cristian Prieto <me@cprieto.com>
2016-05-07 18:55:07 +1000

    Sample project using parser

--- lesson04/Expr.kt
+++ lesson04/Expr.kt
@@ -1,4 +1,15 @@
+import java.io.InputStream
+import java.io.FileInputStream
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.runtime.CommonTokenStream
 
 fun main(args: Array<String>) {
-    println("Hello world")
+    val inputStream = if (args.size > 0) FileInputStream(args[0]) else System.`in`
+    val input = ANTLRInputStream(inputStream)
+    val lexer = ExprLexer(input)
+    val tokens = CommonTokenStream(lexer)
+    val parser = ExprParser(tokens)
+    val tree = parser.prog()
+
+    println(tree.toStringTree(parser))
 }
