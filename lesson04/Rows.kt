9d7f04db6689de0997d670e6fd5445806d66c42c
Cristian Prieto <me@cprieto.com>
2016-05-10 10:41:59 +1000

    Added Rows grammar

---
+++ lesson04/Rows.kt
@@ -0,0 +1,15 @@
+import java.io.FileInputStream
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.runtime.CommonTokenStream
+
+fun main(args: Array<String>) {
+    val inputStream = if (args.size > 0) FileInputStream(args[0]) else System.`in`
+    val input = ANTLRInputStream(inputStream)
+    val lexer = RowsLexer(input)
+    val tokens = CommonTokenStream(lexer)
+    val col = args[1]?.toInt() ?: 0
+    val parser = RowsParser(tokens, col)
+    parser.setBuildParseTree(false)
+
+    parser.file()
+}
